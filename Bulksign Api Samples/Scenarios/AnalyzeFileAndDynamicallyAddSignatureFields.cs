b19b4fdc549c72a3444950134ce01c13bda2fc16
Bulksign <contact@bulksign.com>
2020-06-23 00:49:14 +0300

    updated to latest SDK release (2.3.0)

--- Bulksign Api Samples/Scenarios/AnalyzeFileAndDynamicallyAddSignatureFields.cs
+++ Bulksign Api Samples/Scenarios/AnalyzeFileAndDynamicallyAddSignatureFields.cs
@@ -25,7 +25,8 @@ namespace Bulksign.ApiSamples.Scenarios
 
 			byte[] pfdContent = File.ReadAllBytes(Environment.CurrentDirectory + @"\Files\singlepage.pdf");
 
-			BulksignResult<List<FormFieldResultApiModel>> analyzeResult = api.AnalyzeFile(token, pfdContent);
+			//this will upload the PDF file, analyze it and return a unique file identifier and the PDF file form fields
+			BulksignResult<AnalyzedFileResultApiModel> analyzeResult = api.AnalyzeFile(token, pfdContent);
 
 			BundleApiModel bundle = new BundleApiModel();
 
@@ -49,27 +50,27 @@ namespace Bulksign.ApiSamples.Scenarios
 			};
 
 
-
+			//since we have already sent the PDF document to AnalyzeFile, "reuse it" for the SendBundle request and just pass the file identifier
 			bundle.Documents = new[]
 			{
 				new DocumentApiModel()
 				{
 					FileIdentifier = new FileIdentifier()
 					{
-						//						Identifier = analyzeResult.Response.
+						Identifier = analyzeResult.Response.FileIdentifier
 					}
 				}
 			};
 
 			//assign all form fields / signature field to first recipient
 
-			List<FormFieldResultApiModel> signatures = analyzeResult.Response.Where(model => model.FieldType == FormFieldTypeApi.Signature).ToList();
-
-			List<FormFieldResultApiModel> fields = analyzeResult.Response.Where(model => model.FieldType != FormFieldTypeApi.Signature).ToList();
+			List<FormFieldResultApiModel> signatures = analyzeResult.Response.Fields.Where(model => model.FieldType == FormFieldTypeApi.Signature).ToList();
+			List<FormFieldResultApiModel> fields = analyzeResult.Response.Fields.Where(model => model.FieldType != FormFieldTypeApi.Signature).ToList();
 
 			AssignmentApiModel assignment = new AssignmentApiModel();
 			assignment.AssignedToRecipientEmail = bundle.Recipients[0].Email;
 			assignment.Signatures = new SignatureAssignmentApiModel[signatures.Count];
+
 
 			for (int i = 0; i < signatures.Count; i++)
 			{
@@ -82,25 +83,26 @@ namespace Bulksign.ApiSamples.Scenarios
 				assignment.Fields[i].FieldId = fields[i].Id;
 			}
 
-			bundle.Documents[0].FieldAssignments = new[] {assignment};
+			bundle.Documents[0].FieldAssignments = new[] { assignment };
+
 
 			//now add a signature field for the second recipient on each page of the document
+			int numberPages = analyzeResult.Response.NumberOfPages;
 
-			int numberPages = 4;
-
-			List<NewSignatureApiModel> newSignatures = new	List<NewSignatureApiModel>();
+			List<NewSignatureApiModel> newSignatures = new List<NewSignatureApiModel>();
 
 			for (int i = 1; i <= numberPages; i++)
 			{
-				 NewSignatureApiModel sig = new NewSignatureApiModel();
-				 sig.Height = 100;
-				 sig.Width = 300;
-				 sig.PageIndex = i;
-				 sig.Left = 10;
-				 sig.Top = 100;
-				 sig.SignatureType = SignatureTypeApi.ClickToSign;
+				NewSignatureApiModel sig = new NewSignatureApiModel();
+				sig.Height = 100;
+				sig.Width = 300;
+				sig.PageIndex = i;
+				sig.Left = 10;
+				sig.Top = 100;
+				sig.SignatureType = SignatureTypeApi.ClickToSign;
+				sig.AssignedToRecipientEmail = bundle.Recipients[1].Email;
 
-				 newSignatures.Add(sig);
+				newSignatures.Add(sig);
 			}
 
 			bundle.Documents[0].NewSignatures = newSignatures.ToArray();
