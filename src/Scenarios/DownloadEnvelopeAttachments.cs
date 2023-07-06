00ac4004a935439072e233a6140d88c9f59718a9
Bulksign <contact@bulksign.com>
2023-07-06 15:37:55 +0300

    updated to SDK 4.64 and added samples for 2 new APIs introduced in this release

---
+++ src/Scenarios/DownloadEnvelopeAttachments.cs
@@ -0,0 +1,71 @@
+﻿using Bulksign.Api;
+
+namespace Bulksign.ApiSamples;
+
+public class DownloadEnvelopeAttachments
+{
+	public void RunSample()
+	{
+		const string ENVELOPE_ID = "your_envelope_id";
+
+
+		AuthenticationApiModel token = new Authentication().GetAuthenticationModel();
+
+		if (string.IsNullOrEmpty(token.Key))
+		{
+			Console.WriteLine("Please edit Authentication.cs and set your own API key there");
+			return;
+		}
+
+		BulksignApiClient client = new BulksignApiClient();
+
+		try
+		{
+			BulksignResult<byte[]> result = client.DownloadEnvelopeCompletedAttachments(token, ENVELOPE_ID);
+
+			if (result.IsSuccessful)
+			{
+				//the result here will by a byte[] of a zip file which contains ALL PDF attachments from all the envelope documents
+				Console.WriteLine($"File size :  {result.Result.Length}");
+
+
+				//zip file can be unzipped like this
+				Dictionary<string,byte[]> files = new Utilities().UnzipFiles(result);
+
+
+				//if you need to match the attachment files with the recipient who attach them
+
+
+				//call GetEnvelopeDetails to obtain the envelope information
+				BulksignResult<EnvelopeDetailsResultApiModel> envelopeResult = client.GetEnvelopeDetails(token,ENVELOPE_ID);
+
+				if (!envelopeResult.IsSuccessful)
+				{
+					Console.WriteLine("ERROR : " + envelopeResult.ErrorCode + " " + envelopeResult.ErrorMessage);
+				}
+
+				foreach (RecipientResultApiModel recipient in envelopeResult.Result.Recipients)
+				{
+					if (recipient.Attachments != null)
+					{
+						foreach (CompletedAttachmentApiModel a in recipient.Attachments)
+						{
+							//find the file based on the file name which will be unique
+							KeyValuePair<string,byte[]> pair = files.FirstOrDefault(f => f.Key.ToLower() == a.FileName.ToLower());
+						}
+					}
+				}
+
+			}
+			else
+			{
+				Console.WriteLine("ERROR : " + result.ErrorCode + " " + result.ErrorMessage);
+			}
+		}
+		catch (BulksignException bex)
+		{
+			//handle failed request here. See
+			Console.WriteLine($"Exception {bex.Message}, response is {bex.Response}");
+		}
+	}
+}
---
+++ src/Scenarios/ResetSignatureType.cs
@@ -0,0 +1,52 @@
+﻿using Bulksign.Api;
+namespace Bulksign.ApiSamples;
+
+public class ResetSignatureType
+{
+
+	public void RunSample()
+	{
+		AuthenticationApiModel token = new Authentication().GetAuthenticationModel();
+
+		if (string.IsNullOrEmpty(token.Key))
+		{
+			Console.WriteLine("Please edit Authentication.cs and set your own API key there");
+			return;
+		}
+
+		BulksignApiClient client = new BulksignApiClient();
+
+		try
+		{
+			ResetSignatureTypeApiModel model = new ResetSignatureTypeApiModel()
+			{
+				EnvelopeId = "your_envelope_id",
+
+				//the new type of signature which will be assigned to the recipient
+				SignatureType = Api.ResetSignatureType.ClickToSign,
+
+				//we'll specify the recipient by the index (in this sample being the first recipient)
+				ByIndex = new FindRecipientByIndexApiModel()
+				{
+					Index = 1
+				}
+			};
+
+			BulksignResult<int> result = client.ResetSignatureType(token, model);
+
+			if (result.IsSuccessful)
+			{
+				Console.WriteLine($"{result.Result} signature fields were changed");
+			}
+			else
+			{
+				Console.WriteLine("ERROR : " + result.ErrorCode + " " + result.ErrorMessage);
+			}
+		}
+		catch (BulksignException bex)
+		{
+			//handle failed request here
+			Console.WriteLine($"Exception {bex.Message}, response is {bex.Response}");
+		}
+	}
+}
