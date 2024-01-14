f999202a9afbcebdb19146392b56e43ca00cf1fd
Bulksign <contact@bulksign.com>
2024-01-14 20:19:29 +0200

    added new sample , removed older package

--- src/Scenarios/SendEnvelopeMultipleUploadedFiles.cs
+++ src/Scenarios/SendEnvelopeMultipleUploadedFiles.cs
@@ -3,7 +3,7 @@ using Bulksign.Api;
 
 namespace Bulksign.ApiSamples
 {
-	public class SendEnvelopeDocumentNetworkShare
+	public class SendEnvelopeMultipleUploadedFiles
 	{
 
 		//NOTE : specifying a network path for input files ONLY works on the on-premise version of Bulksign
@@ -20,6 +20,37 @@ namespace Bulksign.ApiSamples
 
 			BulksignApiClient client = new BulksignApiClient();
 
+			//upload individually the PDF documents that will be part of the envelope
+
+			BulksignResult<string> temporaryFile = client.StoreTemporaryFile(token,new FileInput()
+			{
+				FileContent = File.ReadAllBytes(Environment.CurrentDirectory + @"\Files\bulksign_test_Sample.pdf"),
+				Filename    = "bulksign_test_Sample.pdf"
+			});
+
+
+			if (temporaryFile.IsSuccessful == false)
+			{
+				Console.WriteLine("Storing temporary file failed : " + temporaryFile.ErrorMessage);
+				return;
+			}
+
+
+			BulksignResult<string> temporarySecondFile = client.StoreTemporaryFile(token, new FileInput()
+			{
+				FileContent = File.ReadAllBytes(Environment.CurrentDirectory + @"\Files\Sample-Contract-Agreement-Template.pdf"),
+				Filename    = "Sample-Contract-Agreement-Template.pdf"
+			});
+
+			if (temporarySecondFile.IsSuccessful == false)
+			{
+				Console.WriteLine("Storing temporary file failed : " + temporarySecondFile.ErrorMessage);
+				return;
+			}
+
+
+
+
 			EnvelopeApiModel envelope = new EnvelopeApiModel();
 			envelope.EnvelopeType = EnvelopeTypeApi.Serial;
 
@@ -27,28 +58,29 @@ namespace Bulksign.ApiSamples
 			{
 				new RecipientApiModel()
 				{
-					Email = "test@test.com",
+					Email = "contact@bulksign.com",
 					Index = 1,
 					Name = "Test",
 					RecipientType = RecipientTypeApi.Signer
 				}
 			};
 
+			//we specify as input the identifiers of the previously uploaded files 
 			
 			envelope.Documents = new[]
 			{
 				new DocumentApiModel()
 				{
-					FileNetworkShare = new FileNetworkShare()
+					FileIdentifier = new FileIdentifier()
 					{
-						Path = @"\\DocumentShare\\mydocument.pdf"
+						Identifier = temporaryFile.Result
 					}
 				},
 				new DocumentApiModel()
 				{
-					FileNetworkShare = new FileNetworkShare()
+					FileIdentifier = new FileIdentifier()
 					{
-						Path = @"\\DocumentShare\\other.pdf"
+						Identifier = temporarySecondFile.Result
 					}
 				},
 			};
