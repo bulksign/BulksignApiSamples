b3ed5dc23d1a41be160322cba496733f27237c33
Bulksign <contact@bulksign.com>
2024-07-16 13:26:01 +0300

    added new sample for adding a new user

--- src/Scenarios/AddUserToOrganization.cs
+++ src/Scenarios/AddUserToOrganization.cs
@@ -2,7 +2,8 @@ using Bulksign.Api;
 
 namespace Bulksign.ApiSamples;
 
-public class InviteUserToOrganization
+//this sample shows how to add a new user to your organization
+public class AddUserToOrganization
 {
     public void RunSample()
     {
@@ -18,16 +19,23 @@ public class InviteUserToOrganization
 
         try
         {
-
-            UserInvitationApiModel invite = new UserInvitationApiModel()
+            NewUserApiModel invite = new NewUserApiModel()
             {
                 Email = "email@email.com",
                 FirstName = "Test",
                 LastName = "Test",
                 Role = UserRoleApiType.User,
+                
+                UILanguage = "es-ES",   
+                DefaultDraftLanguage = "es-ES",   //specify the default language 
+                
+                NotificationRecipientRejected = true,
+                NotificationEnvelopeCompleted = true,
+                NotificationRecipientOpenedSignStep = false,
+                NotificationRecipientSigned = true          
             };
 
-            BulksignResult<string> result = client.InviteUserToOrganization(token, invite);
+            BulksignResult<string> result = client.AddUserToOrganization(token, invite);
 
             if (result.IsSuccessful == false)
             {
@@ -40,7 +48,7 @@ public class InviteUserToOrganization
         }
         catch (BulksignException bex)
         {
-            //handle failed request here. See
+            //handle failed request here
             Console.WriteLine($"Exception {bex.Message}, response is {bex.Response}");
         }
     }
